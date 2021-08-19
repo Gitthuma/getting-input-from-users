2852418a086063589f1927c62a637e7c63f87e3a
Gitthuma <george.githuma@gmail.com>
2021-08-19 19:42:45 +0300

    Print out a statement with the stored input

--- getting-input-from-users.py
+++ getting-input-from-users.py
@@ -1,3 +1,5 @@
 # Use input with a promt to ask user to input information
 # Store the input in a variable
+# Print out a statement with the stored input
 name = input("Enter name: ")
+print("Welcome " + name + "!")
